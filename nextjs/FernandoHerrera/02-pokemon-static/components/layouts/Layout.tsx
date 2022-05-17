70b838666c63707291e8da96c6e652d3ab7611fe
Jorge Santamaria <jorge.santamaria@lucaedu.com>
2022-05-16 21:39:33 -0300

    Coderhouse Ecommerce Backend

--- nextjs/FernandoHerrera/02-pokemon-static/components/layouts/Layout.tsx
+++ nextjs/FernandoHerrera/02-pokemon-static/components/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 import Head from "next/head";
+import { Navbar } from "../ui";
 
 interface Props {
   title?: string;
@@ -12,13 +13,22 @@ export const Layout: FC<Props> = ({ children, title }) => {
       <Head>
         <title>{title || "PokemonApp"}</title>
         <meta name="author" content="Jorge Santamaria" />
-        <meta name="description" content={`Informacion sobre el Pokemon ${title}`} />
+        <meta
+          name="description"
+          content={`Informacion sobre el Pokemon ${title}`}
+        />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
       </Head>
 
-      {/* <Navbar /> */}
+      <Navbar />
 
-      <main>{children}</main>
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
     </>
   );
 };
--- nextjs/FernandoHerrera/02-pokemon-static/pages/index.tsx
+++ nextjs/FernandoHerrera/02-pokemon-static/pages/index.tsx
@@ -1,13 +1,26 @@
+import { GetStaticProps } from "next";
 import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 import { Layout } from "../components/layouts";
+import pokeApi from "../api/pokeApi";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log({ props });
   return (
     <Layout title="Listado de Pokemons">
       <Button color="gradient">Hola Mundo</Button>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get("/pokemon?limit=150");
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
 };
 
 export default HomePage;
